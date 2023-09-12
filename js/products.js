b807d3a71c1ad1068914b088966b69ae78d10616
Thomy <quevedothomasleonel@gmail.com>
2023-09-12 18:20:06 -0300

    agrego la preentrega 3, solo carro

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Squier Telecaster Jim Root Signature",
+      precio: 5000,
+      img:
+        "https://http2.mlstatic.com/D_NQ_NP_752330-MLA31600858722_072019-O.webp",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Fender Mustang",
+      precio: 10000,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgZuMAvy0foCugE6WN-T6wMWo-NMJFGgs8hA&usqp=CAU",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Ibanez Super Strato White",
+      precio: 15000,
+      img:
+        "https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=B07LFW295H&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=guitarmeet-20",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Gibson Les paul Standard",
+      precio: 20000,
+      img:
+        "https://therockstore.com.ar/wp-content/uploads/2021/10/1621279143283.jpg",
+      cantidad: 1,
+    },
+  ];
